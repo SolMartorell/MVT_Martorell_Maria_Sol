0a6f655be02f168d12ff27438df584e3231207e1
Sol Martorell <msolmartorell@gmail.com>
2022-07-27 19:29:05 -0300

    Arreglo de fecha en views

--- Mi_MVT/familia/views.py
+++ Mi_MVT/familia/views.py
@@ -3,12 +3,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template, Context # loader
-# from datetime import datetime
+from datetime import datetime
 from familia.models import Familia
 
 
 def mama (request):
-    datos = {"nombre": "Cristina", "edad": 72, "fecha_nac": "xxx" }
+    datos = {"nombre": "Cristina", "edad": 72, "fecha_nac": "1950-11-06"}
     archivo  = open(r"C:\Users\msolm\OneDrive\Documents\Curso_Python\VSC\Mi_MVT\Mi_MVT\templates\mama.html", "r")
     contenido = archivo.read()
     archivo.close()
@@ -22,7 +22,7 @@ def mama (request):
     return HttpResponse(documento)
   
 def papa (request):
-    datos = {"nombre": "Juan", "edad": 71, "fecha_nac": "xxx" }
+    datos = {"nombre": "Juan", "edad": 71, "fecha_nac": "1951-08-06" }
     archivo  = open(r"C:\Users\msolm\OneDrive\Documents\Curso_Python\VSC\Mi_MVT\Mi_MVT\templates\papa.html", "r")
     contenido = archivo.read()
     archivo.close()
@@ -36,7 +36,7 @@ def papa (request):
     return HttpResponse(documento)
 
 def hermana (request):
-    datos = {"nombre": "Lucila", "edad": 40, "fecha_nac": "xxx" }
+    datos = {"nombre": "Lucila", "edad": 40, "fecha_nac": "1982-06-11" }
     archivo  = open(r"C:\Users\msolm\OneDrive\Documents\Curso_Python\VSC\Mi_MVT\Mi_MVT\templates\hermana.html", "r")
     contenido = archivo.read()
     archivo.close()
